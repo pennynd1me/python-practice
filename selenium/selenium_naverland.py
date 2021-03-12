9230617c18ecefdec7d913b08f4d498c7846e048
pennynd1me <4prilkim@gmail.com>
2021-03-12 14:03:37 +0900

    article, complex 구분

--- selenium/selenium_naverland.py
+++ selenium/selenium_naverland.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from bs4 import BeautifulSoup
 import time
+from datetime import datetime
 import math
+import re
+import json
 
 serviceUrl = "https://m.land.naver.com/search/result/"
 srcText = input("검색 장소를 입력하세요.")
@@ -10,31 +14,60 @@ url = serviceUrl + srcText
 wd = webdriver.Chrome("chromedriver.exe")
 
 # 창 최대화
+wd.implicitly_wait(10)
 wd.maximize_window()
 wd.get(url)
-
 time.sleep(2)
+# https://m.land.naver.com/cluster/ajax/articleList?&rletTpCd=APT&tradTpCd=A1%3AB1%3AB2&cortarNo=4111300000&page=1
+# https://m.land.naver.com/cluster/ajax/complexList?&rletTpCd=APT&tradTpCd=A1%3AB1%3AB2&cortarNo=4111300000&page=1
+html = wd.page_source
+bs = BeautifulSoup(html, 'html.parser')
+jssource = bs.select_one('#mapSearch > script')
+cortarNo = re.search(r'cortarNo: \'(\d*)\'', jssource.string).group(1)
 
-ele = wd.find_element_by_css_selector("#_countContainer > a.btn_option._complex")
-ele.click()
+pageNm = 1
+resultList = []
+while True :
+    complexurl = f"https://m.land.naver.com/cluster/ajax/complexList?&rletTpCd=APT&tradTpCd=A1%3AB1%3AB2&cortarNo={cortarNo}&page={pageNm}"
+    wd.execute_script(f'window.open("{complexurl}","_blank");')
+    wd.switch_to.window(wd.window_handles[1])
+    html = wd.page_source
+    html = html[84:-20]
+    json_dict = json.loads(html)
+    pageNm += 1
+    time.sleep(0.3)
+    wd.close()
+    resultList.append(json_dict)
+    wd.switch_to.window(wd.window_handles[0])
+    if json_dict['more'] == False:
+        break
 
-wd.implicitly_wait(5)
-howmany = wd.find_element_by_xpath('//*[@id="_listContainer"]/div/div[1]/a/h3/strong')
-rangeNm = math.ceil(int(howmany.text)/20)
-for i in range(rangeNm):
-    eList = wd.find_elements_by_xpath('//*[@id="_listContainer"]/div/div[3]/div/div')
-    action = ActionChains(wd)
-    wd.implicitly_wait(5)
-    time.sleep(1)
-    action.move_to_element(eList[-1]).perform()
-    time.sleep(1)
-# 'SK뷰\n동별매물\n매매5억 3,000~12억 2,000\n전세3억 5,000~5억 8,000\n아파트1018세대총21동2008.11.07.\n79.5㎡ ~ 198.92㎡\n매매22 전세8 월세3'
+with open(f"naver_land_{srcText.replace(' ', '_')}_complex.json", "w", encoding="utf8") as outfile :
+    retJson = json.dumps(resultList, indent=4, sort_keys=True, ensure_ascii=False)
+    outfile.write(retJson)
+print(f"naver_land_{srcText.replace(' ', '_')}_complex.json Saved" )
 
-jsonList = []
-for j in range(len(eList)):
-    json = eList[j].text
-    splited = json.split("\n")
-    splited.insert(0, j+1)
-    print(splited)
+pageNm = 1
+resultList = []
+while True :
+    articleurl = f"https://m.land.naver.com/cluster/ajax/articleList?&rletTpCd=APT&tradTpCd=A1%3AB1%3AB2&cortarNo={cortarNo}&page={pageNm}"
+    wd.execute_script(f'window.open("{articleurl}","_blank");')
+    wd.switch_to.window(wd.window_handles[1])
+    html = wd.page_source
+    html = html[84:-20]
+    json_dict = json.loads(html)
+    pageNm += 1
+    time.sleep(0.3)
+    wd.close()
+    resultList.append(json_dict)
+    wd.switch_to.window(wd.window_handles[0])
+    if json_dict['more'] == False:
+        break
+
+with open(f"naver_land_{srcText.replace(' ', '_')}_article.json", "w", encoding="utf8") as outfile :
+    retJson = json.dumps(resultList, indent=4, sort_keys=True, ensure_ascii=False)
+    outfile.write(retJson)
+print(f"naver_land_{srcText.replace(' ', '_')}_article.json Saved" )
+
 
 a = input()
